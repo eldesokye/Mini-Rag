69f50425892e4be196f681ee72bf352b056dea95
Hesham Eldesoky <hishameldesoky111@gmail.com>
2026-03-15 23:10:30 +0200

    update main.py file and thes the api with fastapi with the baserouter .

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI 
+from dotenv import load_dotenv
+load_dotenv(".env")
+from routes.base import base_router
 
 app = FastAPI(title="Mini-Rag", version="0.1.0", description="A simple RAG (Retrieval-Augmented Generation) application built with FastAPI.")
 
-@app.get("/welcome")
-def welcome_message():
-    return {"message": "Welcome to Mini-Rag! This is a simple RAG application built with FastAPI."}
+app.include_router(base_router)
---
+++ routes/base.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI,APIRouter
+import os 
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"]
+)
+
+@base_router.get("/welcome")
+async def welcome():
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+    
+    return {"message": f"Welcome to {app_name} v{app_version}! This is a simple RAG application built with FastAPI."}
